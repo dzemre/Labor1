68e96fda40073cd015ab08c62f562459b10bbcb2
dzemre <luthiena-dzemre.akcinara@edu.rtu.lv>
2025-02-12 07:54:03 +0000

    laborr

--- LL_2.py
+++ LL_2.py
@@ -21,14 +21,24 @@ class LinkedList:
         self.length += 1
         return True
 
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        
+        return False
     
-    
-    
-    
+
     
 my_linked_list_1 = LinkedList(1)
 my_linked_list_1.append(2)
 my_linked_list_1.append(3)
+my_linked_list_1.append(4)
 my_linked_list_1.append(4)
 my_linked_list_1.tail.next = my_linked_list_1.head
 print(my_linked_list_1.has_loop() ) # Returns True
@@ -39,6 +49,7 @@ print(my_linked_list_1.has_loop() ) # Returns True
 my_linked_list_2 = LinkedList(1)
 my_linked_list_2.append(2)
 my_linked_list_2.append(3)
+my_linked_list_2.append(4)
 my_linked_list_2.append(4)
 print(my_linked_list_2.has_loop() ) # Returns False
 
